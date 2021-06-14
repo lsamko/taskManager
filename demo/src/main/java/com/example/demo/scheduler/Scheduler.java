b4a848277203576c8ba7a5f596efdaecef0871d8
l.samko <liudmyla@backbase.com>
2021-06-14 22:14:51 +0300

    add scheduled tasks

--- demo/src/main/java/com/example/demo/scheduler/Scheduler.java
+++ demo/src/main/java/com/example/demo/scheduler/Scheduler.java
@@ -1,15 +1,29 @@
 package com.example.demo.scheduler;
 
-import com.example.demo.entity.Task;
+import java.util.Date;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Scheduler {
 
-    @Scheduled(cron="0 0 8 * * ?")
-    public void myScheduler(){
+    private static final Logger LOGGER = LoggerFactory.getLogger(Scheduler.class);
 
-        System.out.println("Schedule the job!");
+    @Scheduled(cron = "0 0 8 * * *")
+     void wakeUp()  {
+        LOGGER.info("Time to wake up! " + new Date());
+    }
+
+    @Scheduled(cron = "0 0 17 * * Mon,Wed,Fri")
+    void doWorkout()  {
+        LOGGER.info("Time to exercise! " + new Date());
+    }
+
+    @Scheduled(cron = "0 07 15 * * Wed")
+    void learnSpring()  {
+        LOGGER.info("Time to learn! " + new Date());
     }
 }
